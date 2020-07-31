a2de608713fdfc9878c174a70f2c28ce5cbc3af1
Renato Britto Araujo <renatobritto@protonmail.com>
2020-07-30 22:19:39 -0300

    Fix input style to match old style

--- src/components/userData/InstitutionSelector.js
+++ src/components/userData/InstitutionSelector.js
@@ -9,7 +9,6 @@ import { scale } from '../../utils/scallingUtils'
 import { CheckBox } from 'react-native-elements'
 import ModalSelector from 'react-native-modal-selector'
 import { API_URL } from 'react-native-dotenv';
-//  import Autocomplete from 'react-native-autocomplete-input'
 
 class InstitutionSelector extends Component {
     constructor(props) {
@@ -24,14 +23,25 @@ class InstitutionSelector extends Component {
             userIdCode: null,
             selectedGroup: null,
         }
+    }
+
+    isInputValid() {
+        let isThereSelectedGroup = this.state.selectedGroup != null
+        let doesTheSelectedGroupRequireID = this.state.selectedGroup.require_id
+        let isIdPresentIfNeeded = doesTheSelectedGroupRequireID ? 
+            (this.state.userIdCode != null && this.state.userIdCode.length > 0) : true
+        return isThereSelectedGroup && isIdPresentIfNeeded
+    }
+    
+    setupSelector() {        
         this.getRootGroup()
+        if (this.props.userGroup != null) {
+            console.log("USER HAS A GROUP ALREADY, SELECT NEW")
+        }
     }
     
     updateParent() {
-        let isThereSelectedGroup = this.state.selectedGroup != null
-        let doesTheSelectedGroupRequireID = this.state.selectedGroup.require_id
-        let ifItDoesIsItGiven = doesTheSelectedGroupRequireID ? (this.userIdCode != '' && this.state.userIdCode != null) : true
-        if (isThereSelectedGroup, ifItDoesIsItGiven) {
+        if (this.isInputValid()) {
             console.log("UPDATE PARENT WITH GROUP:", this.state.userGroup, "AND ID CODE:", this.state.userIdCode)
             this.props.setUserInstitutionCallback(this.state.userIdCode, this.state.userGroup)
         }
@@ -60,7 +70,6 @@ class InstitutionSelector extends Component {
     }
 
     getChildren(id) {       
-        console.log("GET CHILDREN", id)
         this.props.setAlert(true)
         fetch(`${API_URL}/groups/${id}/get_children`, {
             method: 'GET',
@@ -127,7 +136,7 @@ class InstitutionSelector extends Component {
 
     groupComponent(group, index) {
         return (
-            <View style={styles.viewChildSexoRaca}>
+            <View style={styles.selectInput}>
                 <Text style={styles.commomTextView}>{this.capitalizeFirstWords(group.label)}:</Text>
                 <ModalSelector
                     initValueTextStyle={{ color: 'black', fontSize: 10 }}
@@ -153,67 +162,77 @@ class InstitutionSelector extends Component {
         )
     }
 
-    showIdentificationCodeInput() {
-        console.log("SHOW IDENTIFICATION CODE", this.state.idenficationCodeInputBox)
-        if (this.state.idenficationCodeInputBox) {
-            return (<View style={styles.viewRow}>
-                <View style={styles.viewChildSexoRaca}>
-                    <Text style={styles.commomTextView}>Nº de Identificação:</Text>
-                    <TextInput style={styles.formInput50}
-                        returnKeyType='done'
-                        keyboardType='number-pad'
-                        onChangeText={(text) => {
-                            console.log("CHANGE TEXT", text)
-                            this.state.userIdCode = text
-                            this.updateParent()
-                        }}
-                    />
+    identificationCodeInput() {
+        return (
+            <View style={styles.selectInput}>
+                <Text style={styles.commomTextView}>Nº de Identificação:</Text>
+                <TextInput style={styles.formInput50}
+                    returnKeyType='done'
+                    keyboardType='number-pad'
+                    onChangeText={(text) => {
+                        this.state.userIdCode = text
+                        this.updateParent()
+                    }}
+                />
+            </View>
+        )
+    }
+
+    rowElements(el1, el2) {
+        return (
+            <View style={styles.modularRow}>
+                <View style={{ flex: 1 }}>
+                    {el1}
                 </View>
-            </View>)
+                <View style={{ flex: 1 }}>
+                    {el2}
+                </View>
+            </View>
+        )
+    }
+
+    groupItemsManager() {
+        const elements = this.groupControl()
+        if (this.state.idenficationCodeInputBox) {
+            elements.push(this.identificationCodeInput())
         }
-        return null
+        if (elements.length == 0) {
+            return null
+        }
+        const rowedElements = []
+        let pair = null
+        for (let el of elements) {
+            if (pair == null) {
+                pair = el
+            } else {
+                rowedElements.push(this.rowElements(pair, el))
+                pair = null
+            }
+        }
+        if (pair != null) {
+            rowedElements.push(this.rowElements(pair, null))
+        }
+        return rowedElements
     }
 
     render() {
         return (
-            <View style={{ marginBottom: 10 }}>
+            <View style={{ marginBottom: 10, ...styles.modularGrid}}>
                 <CheckBox
                     title={"É integrante de alguma instituição de Ensino?"}
                     containerStyle={styles.CheckBoxStyle}
                     size={scale(16)}
                     checked={this.state.groupCheckbox}
                     onPress={() => {
+                        if (!this.state.groupCheckbox && this.state.rootGroup == null) {
+                            this.setupSelector()
+                        }
                         this.setState({ groupCheckbox: !this.state.groupCheckbox })
                     }}
                 />
                 {this.state.groupCheckbox ?
-                    <View>
-                        {this.groupControl()}
-                        {this.showIdentificationCodeInput()}
-
-                        {/* // THIS IS A SEARCH BOX WHERE THE USER MAY TYPE HIS GROUP NAME
-                            // It's not implemented as of yet in the API
-
-                            <View style={ of yet.viewRow}>
-                            <View style={styles.viewChildSexoRaca}>
-                            <Text style={styles.commomTextView}>Instituição:</Text>
-                            <ModalSelector
-                            initValueTextStyle={{ color: 'black', fontSize: 10 }}
-                            style={{ width: '80%', height: '70%' }}
-                            data={getGroups()}
-                            initValue={this.state.initValueGroup}
-                            onChange={(option) => this.setState({ userGroup: option.key, initValueGroup: option.label })}
-                            />
-                            </View>
-                            <View style={styles.viewChildSexoRaca}>
-                            <Text style={styles.commomTextView}>Nº de Identificação:</Text>
-                            <TextInput style={styles.formInput50}
-                            returnKeyType='done'
-                            keyboardType='number-pad'
-                            onChangeText={text => this.setState({ userIdCode: text })}
-                            />
-                            </View>
-                        </View>*/}
+                    <View style={styles.modularColumn}>
+                        {this.groupItemsManager()}
                     </View>
                     : null}
             </View>
@@ -222,19 +241,18 @@ class InstitutionSelector extends Component {
     }
 }
 
-
-// define your styles
 const styles = StyleSheet.create({
-    viewRow: {
-        zIndex: 1,
-        width: '100%',
-        height: 65,
-        flexDirection: 'row',
-    },
-    viewChildSexoRaca: {
-        width: "50%",
+    selectInput: {
+        width: "100%",
         height: 65,
         alignItems: 'center',
+    },
+    modularColumn: {
+        flexDirection: 'column',
+    },
+    modularRow: {
+        flexDirection: 'row',
+        flex: 1
     },
     formInput50: {
         width: "80%",
@@ -245,14 +263,6 @@ const styles = StyleSheet.create({
         paddingBottom: 0,
         paddingTop: 2,
     },
-    commomText: {
-        fontSize: 17,
-        fontFamily: 'roboto',
-        color: '#465F6C',
-        alignSelf: 'flex-start',
-        textAlign: 'left',
-        paddingLeft: "5%",
-    },
     commomTextView: {
         fontSize: 17,
         fontFamily: 'roboto',
@@ -261,55 +271,14 @@ const styles = StyleSheet.create({
         textAlign: 'left',
         paddingLeft: '10%',
     },
-    textCountry: {
-        fontSize: 15,
-        fontFamily: 'roboto',
-    },
     CheckBoxStyle: {
         width: '90%',
         borderRadius: 5,
         borderWidth: 1,
         borderColor: 'rgba(0,0,0,0.11)',
         backgroundColor: 'transparent',
-        //height: scale(32),
         alignSelf: "center"
-    },
-    AutocompleteStyle: {
-        width: '80%',
-        height: 35,
-        fontSize: 14
-    },
-    AutocompleteContainer: {
-        width: "80%",
-        height: 35
-    },
-    AutocompleteList: {
-        borderTopWidth: 0,
-        borderLeftWidth: 0,
-        borderRightWidth: 0,
-        borderBottomWidth: 1,
-        borderBottomColor: '#348EAC'
-    },
-    AutoCompleteListStyles: {
-        borderRadius: 5,
-        backgroundColor: "rgba(218,218,218,0.90)",
-        maxHeight: 150
-    },
-    AutocompleteTouchableOpacity: {
-        position: "relative",
-        zIndex: 2,
-        width: '90%',
-        alignSelf: "center",
-        borderColor: 'rgba(198,198,198,1)',
-        borderBottomWidth: 1,
-    },
-    itemText: {
-        fontSize: 15,
-        marginVertical: 5,
-        fontFamily: 'roboto',
-        color: 'rgba(33,113,245,1)'
     },
 })
 
-//make this component available to the app
 export default InstitutionSelector
