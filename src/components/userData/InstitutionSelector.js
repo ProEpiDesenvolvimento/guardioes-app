f708fce860d7848c06498eb55d04387f0d906cc4
Renato Britto Araujo <renatobritto@protonmail.com>
2020-07-30 21:34:28 -0300

    Refactor InstitutionSelector for new group system

--- src/components/userData/InstitutionSelector.js
+++ src/components/userData/InstitutionSelector.js
@@ -4,64 +4,174 @@ import {
     Text,
     View,
     TextInput,
-    Alert,
 } from 'react-native'
 import { scale } from '../../utils/scallingUtils'
 import { CheckBox } from 'react-native-elements'
 import ModalSelector from 'react-native-modal-selector'
-import { schoolCategory, schoolLocation, educationLevel } from '../../utils/selectorUtils'
-import Autocomplete from 'react-native-autocomplete-input'
 import { API_URL } from 'react-native-dotenv';
+//  import Autocomplete from 'react-native-autocomplete-input'
 
 class InstitutionSelector extends Component {
     constructor(props) {
         super(props)
         this.state = {
             groupCheckbox: false,
-            initValueCity: "Selecionar",
-            initValueGroup: "Selecionar",
-            initValueCategory: "Selecionar",
-            initValueSchoolLocation: "Selecionar",
-            initValueEducationLevel: "Selecionar",
+            groupList: [],
+            selectionIndexes: [],
+            rootGroup: null,
+            idenficationCodeInputBox: false,
+            userGroup: null,
+            userIdCode: null,
+            selectedGroup: null,
+        }
+        this.getRootGroup()
+    }
+    
+    updateParent() {
+        let isThereSelectedGroup = this.state.selectedGroup != null
+        let doesTheSelectedGroupRequireID = this.state.selectedGroup.require_id
+        let ifItDoesIsItGiven = doesTheSelectedGroupRequireID ? (this.userIdCode != '' && this.state.userIdCode != null) : true
+        if (isThereSelectedGroup, ifItDoesIsItGiven) {
+            console.log("UPDATE PARENT WITH GROUP:", this.state.userGroup, "AND ID CODE:", this.state.userIdCode)
+            this.props.setUserInstitutionCallback(this.state.userIdCode, this.state.userGroup)
         }
     }
 
-    updateParent() {
-        this.props.setUserInstitutionCallback(this.state.userIdCode, this.state.userGroup)
-    }
-
-    loadingSchools(Category, Level, City) {
-        const groups = []
-        this.props.setAlert(true)
-        fetch(`${API_URL}/school_units_list/`, {
-            method: 'POST',
+    getRootGroup() {   
+        this.props.setAlert(true) // Set loading popup
+        fetch(`${API_URL}/groups/root`, {
+            method: 'GET',
             headers: {
                 Accept: 'application/vnd.api+json',
                 'Content-Type': 'application/json',
             },
-            body: JSON.stringify({
-                "filter":
-                {
-                    category: Category,
-                    level: Level,
-                    city: City,
-                }
-            })
         })
             .then((response) => {
                 if (response.status === 200) {
-                    setTimeout(() => {
-                        this.props.setAlert(false)
-                      }, 1500);
+                    this.props.setAlert(false)
                     return response.json()
                 }
             })
             .then((responseJson) => {
-                responseJson.school_units.map(group => {
-                    groups.push({ key: group.id, label: group.description })
-                })
-                this.setState({ groups })
+                this.setState({ rootGroup: responseJson.group })
+                this.getChildren(responseJson.group.id)
+                this.props.setAlert(false)
             })
+    }
+
+    getChildren(id) {       
+        console.log("GET CHILDREN", id)
+        this.props.setAlert(true)
+        fetch(`${API_URL}/groups/${id}/get_children`, {
+            method: 'GET',
+            headers: {
+                Accept: 'application/vnd.api+json',
+                'Content-Type': 'application/json',
+            },
+        })
+            .then((response) => {
+                if (response.status === 200) {
+                    return response.json()
+                }
+            })
+            .then((responseJson) => {
+                if (responseJson.is_child) {
+                    this.state.userGroup = id
+                    this.props.setAlert(false)
+                    this.getGroup(id)
+                    return
+                }
+                this.state.selectionIndexes.push({ label: "Selecionar", key: -1 })
+                console.log("PUSHING TO GROUP LIST", responseJson)
+                this.state.groupList.push(responseJson)
+                console.log("PUSHED TO GROUP LIST")
+                this.props.setAlert(false)
+            })
+    }
+
+    getGroup(id) {
+        console.log("GET GROUP", id)
+        this.props.setAlert(true)
+        fetch(`${API_URL}/groups/${id}`, {
+            method: 'GET',
+            headers: {
+                Accept: 'application/vnd.api+json',
+                'Content-Type': 'application/json',
+            },
+        })
+            .then((response) => {
+                if (response.status === 200) {
+                    return response.json()
+                }
+            })
+            .then((responseJson) => {
+                this.state.selectedGroup = responseJson.group
+                console.log(responseJson)
+                if (responseJson.group.require_id) {
+                    console.log("SET ID CODE TRUE")
+                    this.state.idenficationCodeInputBox = true
+                } else {
+                    this.updateParent()
+                }
+                this.props.setAlert(false)
+            })
+    }
+    
+    capitalizeFirstWords(str) {
+        return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+    }
+
+    groupControl() {
+        return this.state.groupList.map((x, i) => this.groupComponent(x, i))
+    }
+
+    groupComponent(group, index) {
+        return (
+            <View style={styles.viewChildSexoRaca}>
+                <Text style={styles.commomTextView}>{this.capitalizeFirstWords(group.label)}:</Text>
+                <ModalSelector
+                    initValueTextStyle={{ color: 'black', fontSize: 10 }}
+                    style={{ width: '80%', height: '70%' }}
+                    data={
+                        group.children.map((x) => {
+                            return {
+                                key: x.id,
+                                label: x.description
+                            }
+                        })
+                    }
+                    initValue={this.state.selectionIndexes[index].label}
+                    onChange={(option) => {
+                        this.state.groupList = this.state.groupList.slice(0, index + 1),
+                        this.state.selectionIndexes = this.state.selectionIndexes.slice(0, index + 1),
+                        this.state.showIdentificationCodeInput = false
+                        this.getChildren(option.key)
+                        this.state.selectionIndexes[index] = option
+                    }}
+                />
+            </View>
+        )
+    }
+
+    showIdentificationCodeInput() {
+        console.log("SHOW IDENTIFICATION CODE", this.state.idenficationCodeInputBox)
+        if (this.state.idenficationCodeInputBox) {
+            return (<View style={styles.viewRow}>
+                <View style={styles.viewChildSexoRaca}>
+                    <Text style={styles.commomTextView}>Nº de Identificação:</Text>
+                    <TextInput style={styles.formInput50}
+                        returnKeyType='done'
+                        keyboardType='number-pad'
+                        onChangeText={(text) => {
+                            console.log("CHANGE TEXT", text)
+                            this.state.userIdCode = text
+                            this.updateParent()
+                        }}
+                    />
+                </View>
+            </View>)
+        }
+        return null
     }
 
     render() {
@@ -72,93 +182,19 @@ class InstitutionSelector extends Component {
                     containerStyle={styles.CheckBoxStyle}
                     size={scale(16)}
                     checked={this.state.groupCheckbox}
-                    onPress={() => this.setState({ groupCheckbox: !this.state.groupCheckbox })}
+                    onPress={() => {
+                        this.setState({ groupCheckbox: !this.state.groupCheckbox })
+                    }}
                 />
                 {this.state.groupCheckbox ?
                     <View>
-                        <View style={styles.viewRow}>
-                            <View style={styles.viewChildSexoRaca}>
-                                <Text style={styles.commomTextView}>Categoria:</Text>
-                                <ModalSelector
-                                    initValueTextStyle={{ color: 'black', fontSize: 10 }}
-                                    style={{ width: '80%', height: '70%' }}
-                                    data={schoolCategory}
-                                    initValue={this.state.initValueCategory}
-                                    onChange={(option) => { if (option.key == "UNB") { this.loadingSchools(option.key) } this.setState({ userCategory: option.key, initValueCategory: option.label, userEducationLevel: null }) }}
-                                />
-                            </View>
-                            {this.state.userCategory == "UNB" ?
-                                <View style={styles.viewChildSexoRaca}>
-                                    <Text style={styles.commomTextView}>Faculdade:</Text>
-                                    <ModalSelector
-                                        initValueTextStyle={{ color: 'black', fontSize: 10 }}
-                                        style={{ width: '80%', height: '70%' }}
-                                        data={this.state.groups}
-                                        initValue={this.state.initValueGroup}
-                                        onChange={async (option) => {
-                                            await this.setState({ userGroup: option.key, initValueGroup: option.label })
-                                            this.updateParent()
-                                        }}
-                                    />
-                                </View>
-                                : this.state.userCategory == "SES-DF" ?
-                                    <View style={styles.viewChildSexoRaca}>
-                                        <Text style={styles.commomTextView}>Nivel de Ensino:</Text>
-                                        <ModalSelector
-                                            initValueTextStyle={{ color: 'black', fontSize: 10 }}
-                                            style={{ width: '80%', height: '70%' }}
-                                            data={educationLevel}
-                                            initValue={this.state.initValueEducationLevel}
-                                            onChange={(option) => this.setState({ userEducationLevel: option.key, initValueEducationLevel: option.label })}
-                                        />
-                                    </View>
-                                    : null}
-                        </View>
-                        {this.state.userEducationLevel != null ?
-                            <View style={styles.viewRow}>
-                                <View style={styles.viewChildSexoRaca}>
-                                    <Text style={styles.commomTextView}>Região:</Text>
-                                    <ModalSelector
-                                        initValueTextStyle={{ color: 'black', fontSize: 10 }}
-                                        style={{ width: '80%', height: '70%' }}
-                                        data={schoolLocation}
-                                        initValue={this.state.initValueSchoolLocation}
-                                        onChange={(option) => { this.loadingSchools(this.state.userCategory, this.state.userEducationLevel, option.key), this.setState({ userSchoolLocation: option.key, initValueSchoolLocation: option.label }) }}
-                                    />
-                                </View>
-                                {this.state.userSchoolLocation != null ?
-                                    <View style={styles.viewChildSexoRaca}>
-                                        <Text style={styles.commomTextView}>Unidade:</Text>
-                                        <ModalSelector
-                                            initValueTextStyle={{ color: 'black', fontSize: 10 }}
-                                            style={{ width: '80%', height: '70%' }}
-                                            data={this.state.groups}
-                                            initValue={this.state.initValueGroup}
-                                            onChange={async (option) => {
-                                                await this.setState({ userGroup: option.key, initValueGroup: option.label })
-                                                this.updateParent()
-                                            }}
-                                        />
-                                    </View>
-                                    : null}
-                            </View>
-                            : this.state.userGroup != null && this.state.userCategory == "UNB" ?
-                                <View style={styles.viewRow}>
-                                    <View style={styles.viewChildSexoRaca}>
-                                        <Text style={styles.commomTextView}>Nº de Identificação:</Text>
-                                        <TextInput style={styles.formInput50}
-                                            returnKeyType='done'
-                                            keyboardType='number-pad'
-                                            onChangeText={async (text) => {
-                                                await this.setState({ userIdCode: text })
-                                                this.updateParent()
-                                            }}
-                                        />
-                                    </View>
-                                </View>
-                                : null}
+                        {this.groupControl()}
+                        {this.showIdentificationCodeInput()}
 
-                        {/*<View style={styles.viewRow}>
+                        {/* // THIS IS A SEARCH BOX WHERE THE USER MAY TYPE HIS GROUP NAME
+                            // It's not implemented as of yet in the API
+
+                            <View style={ of yet.viewRow}>
                             <View style={styles.viewChildSexoRaca}>
                             <Text style={styles.commomTextView}>Instituição:</Text>
                             <ModalSelector
