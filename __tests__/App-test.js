a0bbee1bcb56e9c0b23ae8a8251de2060626579d
Fabiana Ribas <fabianalv.ribas@gmail.com>
2020-10-01 19:31:21 -0300

    Fix stages and generating test keystore
    Signed-off-by: Fabiana Ribas <fabianalv.ribas@gmail.com>
    
    Co-authored-by: Gabriela Medeiros <gabriela.medeiros2010@hotmail.com.br>

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 test('Example', () => {
-  expect(3).toBe(3);
-});
+  expect(3).toBe(3)
+})
--- deploy.sh
+++
@@ -1,4 +0,0 @@
-npx jetify
-cd android && ./gradlew assembleRelease
-cp app/build/outputs/apk/release/app-release.apk ..
-cd ..
