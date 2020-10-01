4292ee8796fab43362ce709e56615418f27aeb1c
Gabriela Medeiros <gabriela.medeiros2010@hotmail.com.br>
2020-10-01 07:09:09 -0300

    Upadating eslint version

--- .eslintrc.js
+++ .eslintrc.js
@@ -28,16 +28,9 @@ module.exports = {
     'prettier/prettier': 'error',
     'react/jsx-filename-extension': [
       'warn',
-      {
-        extensions: ['.jsx', '.js']
-      }
+      {extensions: ['.js', '.jsx']}
     ],
     'import/prefer-default-export': 'off',
-    'react/state-in-constructor': 'off',
-    'react/static-property-placement': 'off',
-    'react/jsx-props-no-spreading': 'off',
-    'react/prop-types': 'off',
-    'no-param-reassign': 'off',
-    'no-console': 'off'
-  },
+    'jsx-quotes': ['error', 'prefer-single']
+  }
 };
