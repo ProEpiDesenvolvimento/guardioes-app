24176cdf0564ea18c9f6a3a4356652ddd48c2cde
Ítalo Alves <italooko@gmail.com>
2021-04-08 17:46:10 -0300

    Fix splashscreen on iOS

--- ios/guardioesapp/AppDelegate.m
+++ ios/guardioesapp/AppDelegate.m
@@ -6,6 +6,7 @@
  */
 
 #import "AppDelegate.h"
+#import "RNSplashScreen.h"
 
 #import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
@@ -27,6 +28,7 @@
   rootViewController.view = rootView;
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
+  [RNSplashScreen show];
   return YES;
 }
 
