11797126ebe63eb2b0e4275f83fa84eceff153f5
João Pedro <jps12carvalho@gmail.com>
2021-05-12 12:47:17 -0300

    Styling Autocopmlete component

--- src/components/Autocomplete/index.js
+++ src/components/Autocomplete/index.js
@@ -1,18 +1,26 @@
 import React, { useState } from "react";
-import { View, TextInput } from "react-native"
+import { Button } from "react-native";
+import { ScrollView } from "react-native";
+import { Pressable } from "react-native";
+import { View, TextInput, StyleSheet, Text } from "react-native"
 import {
   AutocompleteModal,
   CancelButton,
   ModalView,
   TextModalView,
-  AutocompleteButton,
+  AutocompleteText,
+  TextInputModal,
+  ScrollModalView,
+  ModalFade,
 } from "./styles"
 
 
 const Autocomplete = (props) => {
   const [modalVisible, setModalVisible] = useState(false);
-  const [value, setValue] = useState(props.value)
-
+  const [value, setValue] = useState(props.value);
+  const [data, setData] = useState(props.data.filter((data) => {
+    return !(data.label === undefined)
+  }))
   return (
     <View>
       <AutocompleteModal
@@ -21,30 +29,34 @@ const Autocomplete = (props) => {
           setModalVisible(!modalVisible);
         }}
       >
-        <ModalView>
-          <TextInput onChangeText={setValue} value={value} />
-          {props.data.filter((data) => {
-            return !(data.toLowerCase().indexOf(value.toLowerCase()) === -1);
-          }).map((data, index) => {
-            return (
-              <TextModalView key={index} onPress={() => {
-                props.onChange(data)
-                setModalVisible(false)
-              }}>
-                {data.toUpperCase()}
-              </TextModalView>
-            )
-          })}
-        </ModalView>
-        <CancelButton
-          onPress={() => setModalVisible(false)}
-          title="Cancelar"
-        />
-        <AutocompleteButton
-          title={props.value}
-          onPress={() => setModalVisible(true)}
-        />
+        <ModalFade>
+
+          <ModalView>
+            <TextInputModal onChangeText={setValue} value={value} placeholder="Pesquisa" />
+            <ScrollModalView>
+              {data.filter((data) => {
+                if (data.label === undefined) return false
+                return !(data.label.toLowerCase().indexOf(value.toLowerCase()) === -1);
+              }).map((data, index) => {
+                return (
+                  <TextModalView key={index} onPress={() => {
+                    props.onChange(data.label)
+                    setModalVisible(false)
+                  }}>
+                    {data.label.toUpperCase()}
+                  </TextModalView>
+                )
+              })}
+            </ScrollModalView>
+            <CancelButton
+              onPress={() => setModalVisible(false)}
+            >{props.cancelText}</CancelButton>
+          </ModalView>
+        </ModalFade>
       </AutocompleteModal>
+      <AutocompleteText
+        onPress={() => setModalVisible(true)}
+      >{props.value}</AutocompleteText>
     </View>
   )
 }
--- src/components/Autocomplete/styles.js
+++ src/components/Autocomplete/styles.js
@@ -3,32 +3,65 @@ import { scale } from '../../utils/scalling'
 
 export const ModalView = styled.View`
     margin: auto;
-    backgroundColor: white;
-    borderRadius: ${scale(12)};
+    backgroundColor: transparent;
+    borderRadius: ${scale(12)}px;
     width: 90%;
     alignItems: center;
     color: #32323B;
     height: auto;
 `
 
+export const ModalFade = styled.View`
+    background-color: 'rgba(0, 0, 0, 0.6)';
+    height: 100%;
+`
+
+export const TextInputModal = styled.TextInput`
+    background-color: white;
+    fontFamily: 'ArgentumSans';
+    borderRadius: ${scale(12)}px;
+    width: 100%;
+    text-align: center; 
+`
+
+export const ScrollModalView = styled.ScrollView`
+    background-color: white;
+    width: 100%;
+    margin-top: ${scale(10)}px;
+    borderRadius: ${scale(12)}px;
+    max-height: 70%
+`
+
 export const TextModalView = styled.Text`
     fontFamily: 'ArgentumSans';
     color: #32323B;
-    fontSize: ${scale(14)};
+    padding: ${scale(10)}px;
+    fontSize: ${scale(14)}px;
+    text-align: center;
 `
 
-export const CancelButton = styled.Button`
-    backgroundColor: #ffffff,
-    borderRadius: ${scale(14)},
-    marginTop: ${scale(10)},
+export const CancelButton = styled.Text`
+    background-color: #348eac;
+    color: white;
+    padding: ${scale(10)}px;
+    borderRadius: ${scale(14)}px;
+    marginTop: ${scale(10)}px;
+    width: 100%
+    font-family: 'ArgentumSans';
+    text-align: center;
 `
 
 export const AutocompleteModal = styled.Modal.attrs({
     animationType: "slide",
     transparent: true,
 })`
-    width: 90%
 `
 
-export const AutocompleteButton = styled.Button`
+export const AutocompleteText = styled.Text`
+    background-color: white;
+    font-family: 'ArgentumSans';
+    border-radius: ${scale(14)}px;
+    height: ${scale(30)}px;
+    text-align: center;
+    width: 90%;
 `
