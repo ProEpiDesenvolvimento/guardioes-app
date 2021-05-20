2789e9abf51d186fa926918237f3a16f639d9191
João Pedro <jps12carvalho@gmail.com>
2021-05-20 20:42:39 -0300

    Fixing iO bugs

--- src/navigation/HomeDrawer.js
+++ src/navigation/HomeDrawer.js
@@ -5,22 +5,25 @@ import LeftMenu from '../components/LeftMenu'
 import TabBar from './TabBar'
 
 import { scale } from '../utils/scalling'
+import { SafeAreaProvider } from 'react-native-safe-area-context'
 
 const Drawer = createDrawerNavigator()
 
 const HomeDrawer = () => {
     return (
-        <Drawer.Navigator
-            drawerContent={({ navigation }) => (
-                <LeftMenu navigation={navigation} />
-            )}
-            drawerStyle={{
-                backgroundColor: 'transparent',
-                width: scale(290),
-            }}
-        >
-            <Drawer.Screen name='TabBar' component={TabBar} />
-        </Drawer.Navigator>
+        <SafeAreaProvider>
+            <Drawer.Navigator
+                drawerContent={({ navigation }) => (
+                    <LeftMenu navigation={navigation} />
+                )}
+                drawerStyle={{
+                    backgroundColor: 'transparent',
+                    width: scale(290),
+                }}
+            >
+                <Drawer.Screen name='TabBar' component={TabBar} />
+            </Drawer.Navigator>
+        </SafeAreaProvider>
     )
 }
 
--- src/screens/principal/Home/index.js
+++ src/screens/principal/Home/index.js
@@ -168,7 +168,7 @@ const Home = ({ navigation }) => {
 
         const userLastSurveys = surveys.filter(
             (survey) =>
-                new Date(survey.created_at).getTime() >= lastWeek.getTime()
+                survey && new Date(survey.created_at).getTime() >= lastWeek.getTime()
         )
 
         let badReports = 0
