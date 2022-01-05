e38b2781fa9334630b3c284e58ddb74ffb0972d3
clarice42 <claramaia820@gmail.com>
2022-01-05 20:23:01 -0300

    Add api requests to get and create doses

--- src/api/vaccines.js
+++ src/api/vaccines.js
@@ -4,7 +4,7 @@ export const getVaccines = async (token) => {
     let response = {}
 
     try {
-        response = await fetch(`${API_URL}/vaccines`, {
+        response = await fetch(`http://localhost:3001/vaccines`, {
             headers: {
                 Accept: 'application/vnd.api+json',
                 Authorization: token,
@@ -17,5 +17,52 @@ export const getVaccines = async (token) => {
     return {
         status: response.status,
         body: response.status === 200 ? await response.json() : null,
+    }
+}
+
+export const getDoses = async (token) => {
+    let response = {}
+
+    try {
+        response = await fetch(`http://localhost:3001/doses`, {
+            headers: {
+                Accept: 'application/vnd.api+json',
+                Authorization: token,
+            },
+        })
+    } catch (err) {
+        console.log(err)
+    }
+
+    return {
+        status: response.status,
+        body: response.status === 200 ? await response.json() : null,
+    }
+}
+
+export const sendDose = async (data, token) => {
+    let response = {}
+
+    try {
+        response = await fetch(`http://localhost:3001/doses`, {
+            method: 'POST',
+            headers: {
+                Accept: 'application/vnd.api+json',
+                'Content-Type': 'application/json',
+                Authorization: token,
+            },
+            body: JSON.stringify({
+                date: data.date,
+                dose: data.dose,
+                vaccine_id: data.vaccine_id,
+            }),
+        })
+    } catch (err) {
+        console.log(err)
+    }
+
+    return {
+        status: response.status,
+        body: await response.json(),
     }
 }
