36b6f4520dac4f74fb61325676344baa4aadf3d4
clarice42 <claramaia820@gmail.com>
2022-01-10 21:09:53 -0300

    Fix request urls

--- src/api/vaccines.js
+++ src/api/vaccines.js
@@ -4,7 +4,7 @@ export const getVaccines = async (token) => {
     let response = {}
 
     try {
-        response = await fetch(`http://localhost:3001/vaccines`, {
+        response = await fetch(`${API_URL}/vaccines`, {
             headers: {
                 Accept: 'application/vnd.api+json',
                 Authorization: token,
@@ -24,7 +24,7 @@ export const getDoses = async (token) => {
     let response = {}
 
     try {
-        response = await fetch(`http://localhost:3001/doses`, {
+        response = await fetch(`${API_URL}/doses`, {
             headers: {
                 Accept: 'application/vnd.api+json',
                 Authorization: token,
@@ -44,7 +44,7 @@ export const sendDose = async (data, token) => {
     let response = {}
 
     try {
-        response = await fetch(`http://localhost:3001/doses`, {
+        response = await fetch(`${API_URL}/doses`, {
             method: 'POST',
             headers: {
                 Accept: 'application/vnd.api+json',
