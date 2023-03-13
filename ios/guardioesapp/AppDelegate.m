47eeefc355a9fbda8df52b9d2c7126b1d39286b6
Ítalo Alves <italooko@gmail.com>
2023-03-13 15:23:22 -0300

    Add deep linking to iOS

--- ios/guardioesapp/AppDelegate.m
+++ ios/guardioesapp/AppDelegate.m
@@ -11,6 +11,7 @@
 #import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
+#import <React/RCTLinkingManager.h>
 
 @implementation AppDelegate
 
@@ -41,6 +42,13 @@
 #else
   return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
 #endif
+}
+
+- (BOOL)application:(UIApplication *)application
+   openURL:(NSURL *)url
+   options:(NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options
+{
+  return [RCTLinkingManager application:application openURL:url options:options];
 }
 
 @end
