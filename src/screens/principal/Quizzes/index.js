a874eff9a986b9122d8967cea4cd0ff181719fd4
Ítalo Alves <italooko@gmail.com>
2023-11-01 14:26:11 -0300

    Quiz bug fixes

--- src/screens/principal/Quizzes/index.js
+++ src/screens/principal/Quizzes/index.js
@@ -4,7 +4,9 @@ import { useFocusEffect } from '@react-navigation/native'
 
 import Feather from 'react-native-vector-icons/Feather'
 import ScreenLoader from '../../../components/ScreenLoader'
-import { Help, Box, Button, IconWrapper, InfoWrapper, Title } from './styles'
+import { Container } from '../../../components/NormalForms'
+import { ScrollViewStyled } from '../../../components/PageItems'
+import { Box, Button, IconWrapper, InfoWrapper, Title } from './styles'
 
 import translate from '../../../../locales/i18n'
 import { useUser } from '../../../hooks/user'
@@ -24,22 +26,27 @@ const Quizzes = ({ navigation }) => {
     const [quizzes, setQuizzes] = useState([])
 
     const getQuizIconName = (quiz) => {
-        const formData = quiz.flexible_form_version.data
+        const formVersion = quiz.flexible_form_version
 
-        if (quiz.done) {
+        if (quiz.tried && quiz.done) {
             return 'check-square'
         }
-        if (formData) {
-            const startDate = moment(formData.start_date)
-            const endDate = moment(formData.end_date)
-            const now = moment().local()
+        if (formVersion.data) {
+            const startDate = moment(formVersion.data.start_date)
+            const endDate = moment(formVersion.data.end_date)
+            const currentDate = moment(formVersion.current_date)
 
-            if (now.isBetween(startDate, endDate)) {
+            const isBetweenDates = currentDate.isBetween(startDate, endDate)
+
+            if (isBetweenDates) {
                 return 'unlock'
+            }
+            if (!isBetweenDates && quiz.tried) {
+                return 'square'
             }
             return 'lock'
         }
-        return 'unlock'
+        return 'lock'
     }
 
     const formattedQuizzes = () => {
@@ -71,10 +78,13 @@ const Quizzes = ({ navigation }) => {
                 }
             }
 
+            // tried: if the user has already answered the quiz
+            // done: if the user has answered all the questions of the quiz
             newQuizzes.push({
                 ...form,
                 percentage: answer?.data.percentage || 0,
-                done: answers.length > 0,
+                tried: answers.length > 0,
+                done: answer?.data.answers?.length > 0,
             })
         })
 
@@ -125,34 +135,46 @@ const Quizzes = ({ navigation }) => {
     }
 
     return (
-        <Help>
-            {quizzes.map((quiz) => {
-                const iconName = getQuizIconName(quiz)
-                return (
-                    <Button
-                        key={quiz.id}
-                        onPress={() => navigation.navigate('Quiz', { quiz })}
-                        disabled={
-                            iconName === 'lock' || iconName === 'check-square'
-                        }
-                    >
-                        <Box>
-                            <IconWrapper>
-                                <Feather
-                                    name={iconName}
-                                    size={scale(26)}
-                                    color='#348eac'
-                                />
-                            </IconWrapper>
-                            <InfoWrapper>
-                                <Title>{quiz.title}</Title>
-                                <Title>{quiz.percentage.toFixed(0)}/100</Title>
-                            </InfoWrapper>
-                        </Box>
-                    </Button>
-                )
-            })}
-        </Help>
+        <Container>
+            <ScrollViewStyled>
+                {quizzes.map((quiz) => {
+                    const formData = quiz.flexible_form_version.data
+                    const iconName = getQuizIconName(quiz)
+
+                    if (!formData?.visible) {
+                        return null
+                    }
+                    return (
+                        <Button
+                            key={quiz.id}
+                            onPress={() =>
+                                navigation.navigate('Quiz', { quiz })
+                            }
+                            disabled={
+                                iconName === 'check-square' ||
+                                iconName === 'lock'
+                            }
+                        >
+                            <Box>
+                                <IconWrapper>
+                                    <Feather
+                                        name={iconName}
+                                        size={scale(26)}
+                                        color='#348eac'
+                                    />
+                                </IconWrapper>
+                                <InfoWrapper>
+                                    <Title>{quiz.title}</Title>
+                                    <Title>
+                                        {quiz.percentage.toFixed(0)}/100
+                                    </Title>
+                                </InfoWrapper>
+                            </Box>
+                        </Button>
+                    )
+                })}
+            </ScrollViewStyled>
+        </Container>
     )
 }
 
