1d461fb0ce63c45fa53f6d7401c3d45bb9eeda79
Ítalo Alves <italooko@gmail.com>
2024-05-12 19:21:54 -0300

    Change signal answers sorting

--- src/screens/professional/SignalAnswers/index.js
+++ src/screens/professional/SignalAnswers/index.js
@@ -64,6 +64,12 @@ const SignalAnswers = ({ navigation }) => {
         })
 
         const mergedFlexibleAnswers = [...parsedAnswers, ...pendingAnswers]
+        mergedFlexibleAnswers.sort(
+            (a, b) =>
+                new Date(b.updated_at).getTime() -
+                new Date(a.updated_at).getTime()
+        )
+
         getGroupedAnswers(mergedFlexibleAnswers)
     }
 
