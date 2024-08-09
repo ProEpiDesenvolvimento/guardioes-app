e88b5ed5647107a539315cdce886b14432bba527
Ítalo Alves <italooko@gmail.com>
2024-08-09 12:40:56 -0300

    Add Cabo Verde phone mask

--- src/utils/masks.js
+++ src/utils/masks.js
@@ -1,4 +1,4 @@
-export const maskPhone = (phone) => {
+export const maskPhoneBrazil = (phone) => {
     let masked = phone
     masked = masked.replace(/\D/g, '')
     // (11) 11111-1111 or (111) 11111-1111
@@ -9,6 +9,29 @@ export const maskPhone = (phone) => {
     }
     masked = masked.replace(/(\d)(\d{4})$/, '$1-$2')
     return masked
+}
+
+export const maskPhoneCaboVerde = (phone) => {
+    let masked = phone.replace(/\D/g, '')
+
+    if (masked.length > 11) {
+        masked = masked.slice(0, 11)
+    }
+    if (masked.length > 3) {
+        masked = `+238 ${masked.slice(3, 6)} ${masked.slice(6, 10)}`
+    } else {
+        masked = `+238 ${masked.slice(3)}`
+    }
+
+    masked = masked.trim()
+    return masked
+}
+
+export const maskPhone = (country, phone) => {
+    if (country === 'Cabo Verde') {
+        return maskPhoneCaboVerde(phone)
+    }
+    return maskPhoneBrazil(phone)
 }
 
 export const maskIdentificationCode = (code) => {
